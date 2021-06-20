72adea94accab04dff266af7e5a66eb1e608ba3c
Inga Pflaumer <inga.pflaumer@gmail.com>
2021-06-20 23:15:41 +1000

    Connections generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,116 @@
+import { debug } from "console";
 import React from "react";
 import "./App.css";
 import { ICell } from "./Figth/Cell";
 import { Table } from "./Figth/Table";
-import { shuffle } from "./utils/helpers";
+import { generateInt, shuffle } from "./utils/helpers";
 import { allRooms } from "./utils/roomsBase1";
+const maxRoomNumber = 10;
 
-const generateDungeon = (): ICell[][] => {
-  const newRooms = [];
-  const randomRooms = shuffle(allRooms).slice(0, 10);
+interface IConnection {
+  from: number;
+  to: number;
+}
+const generateDungeon = (): ICell[][][] => {
+  const randomRooms = shuffle(allRooms).slice(0, maxRoomNumber);
+  return randomRooms;
+};
 
-  for (let i = 0; i < randomRooms.length; i++) {
-    const newRoom = randomRooms[i].concat([[{ type: "connect" }]]);
+const generateConnections = (rooms: ICell[][][]): IConnection[] => {
+  const connections = [{ from: 0, to: 1 }];
+  while (true) {
+    const unconnected = findUnconnected(connections);
+    if (unconnected.length === 0) {
+      break;
+    }
+    const randomRoom1 = unconnected[generateInt(unconnected.length)];
+    let randomRoom2 = generateInt(maxRoomNumber);
+    while (
+      randomRoom1 === randomRoom2 ||
+      connections.find((c) => c.from === randomRoom1 && c.to === randomRoom2)
+    ) {
+      randomRoom2 = generateInt(maxRoomNumber);
+    }
+    connections.push({ from: randomRoom1, to: randomRoom2 });
+  }
+  return connections;
+};
 
-    if (i === randomRooms.length - 1) {
-      newRooms.push(randomRooms[i]);
-    } else {
-      newRooms.push(newRoom);
+const findUnconnected = (connections: IConnection[]): number[] => {
+  // setup
+  const visited: number[] = [];
+  const toVisit: number[] = [0];
+
+  // search step
+  do {
+    const nextRoom = toVisit.pop();
+    if (nextRoom === undefined) {
+      break;
+    }
+    visited.push(nextRoom);
+    const nextRooms: number[] = connectedRooms(nextRoom, connections);
+    const newRooms = nextRooms.filter((n) => !visited.includes(n));
+    toVisit.push(...newRooms);
+  } while (toVisit.length > 0);
+  const unconnected = [];
+  for (let i = 0; i < maxRoomNumber; i++) {
+    if (!visited.includes(i)) {
+      unconnected.push(i);
     }
   }
-  return newRooms.flat();
+
+  return unconnected;
+};
+
+const connectedRooms = (room: number, connections: IConnection[]): number[] => {
+  const rooms: number[] = [];
+  for (const connection of connections) {
+    if (connection.from === room && !rooms.includes(connection.to)) {
+      rooms.push(connection.to);
+    }
+    if (connection.to === room && !rooms.includes(connection.from)) {
+      rooms.push(connection.from);
+    }
+  }
+  return rooms;
+};
+
+//   do {
+//     let notVisited = rooms.flat();
+//     let visited: ICell[][] = [];
+//     let toVisit = [notVisited[0]];
+//     const roomConnections = connections.filter(
+//       (c: IConnection) => c.to === 0 || c.from === 0
+//     );
+//     roomConnections.forEach((c: IConnection) => {
+//       const entryInd = c.from;
+//       const exitInd = c.to;
+
+//     });
+//   } while (true);
+
+//   return connections;
+// };
+
+export const Debug = ({ data }: { data: IConnection[] }) => {
+  return (
+    <div>
+      {data.map((d: IConnection) => (
+        <p>{`from ${d.from} to ${d.to}`}</p>
+      ))}
+    </div>
+  );
 };
 
 function App() {
-  const data = generateDungeon();
-  console.log(data);
+  const rooms = generateDungeon();
+  const coonections = generateConnections(rooms);
+  console.log("Rooms", rooms.length, rooms);
+  console.log("Connections", coonections.length);
   return (
     <div className="App">
-      <Table data={data} />
+      <Debug data={coonections} />
+      <Table data={rooms.flat()} />
     </div>
   );
 }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,17 +1,28 @@
 //@ts-ignore
 export const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export const unique = (arrArg: any[]) =>
+export const uniqueArray = (arrArg: any[]) =>
   arrArg.filter(
     (elem: any, pos: number, arr: any) => arr.indexOf(elem) === pos
   );
+
+export const uniqueItem = (arr: any, item: any) => {
+  return arr.indexOf(item) === -1;
+};
+
+export const itemIndex = (arr: any, item: any) => {
+  return arr.indexOf(item);
+};
 
 export const generateInt = (max: number) =>
   Math.floor(Math.random() * Math.floor(max));
 
 export const shuffle = (array: any) => array.sort(() => Math.random() - 0.5);
 
-export const exists = (actors: any, { x, y }: { x: number; y: number }) => {
+export const existsInRoom = (
+  actors: any,
+  { x, y }: { x: number; y: number }
+) => {
   const result = actors.some(
     (c: any) => c.position.x === x && c.position.y === y
   );
